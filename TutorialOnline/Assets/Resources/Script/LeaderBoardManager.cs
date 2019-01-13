9c523dc873e8fe8c40e95c140447f5d76cf08d80
unich-mizuki <45304868+unich-mizuki@users.noreply.github.com>
2019-01-13 14:10:08 +0900

    2019/01/13 haraguchi

---
+++ TutorialOnline/Assets/Resources/Script/LeaderBoardManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LeaderBoardManager : MonoBehaviour
+{
+
+    // private LeaderBoard lBoard;
+    public Text targetText;
+   // public GameObject[] top = new GameObject[5];
+   // public GameObject[] nei = new GameObject[5];
+
+    void Start()
+    {
+ //       lBoard = new LeaderBoard();
+
+        // テキストを表示するゲームオブジェクトを取得
+         //for (int i = 0; i < 5; ++i)
+         //{
+   //          top[i] = GameObject.Find("Top" + i);
+     //        nei[i] = GameObject.Find("Neighbor" + i);
+       //  }
+        for (int i = 0; i < 5; ++i)
+        {
+
+            //   top[i].guiText.text = i + 1 + ". " + lBoard.topRankers[i].print();
+            this.targetText = this.GetComponent<Text>(); // <---- 追加3
+            this.targetText.text = "ChangeText";
+        }
+
+
+    }
+
+}
